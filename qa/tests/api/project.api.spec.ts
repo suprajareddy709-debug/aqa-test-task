8c097e1070ac2e0a76d32f014c6714c9b9942b0b
suprajareddy709 <suprajareddy709@gmail.com>
2026-04-18 20:28:23 +0530

    update code for ci

--- qa/tests/api/project.api.spec.ts
+++ qa/tests/api/project.api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../../fixture.js';
+import { test, expect } from '../../fixture';
 import { request } from  '@playwright/test'
 
 
--- qa/tests/ui/login.spec.ts
+++ qa/tests/ui/login.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../../fixture.js';
+import { test, expect } from '../../fixture';
 
 test.describe('Authentication Tests', { tag: ['@Vikunja'] }, () => {
 
